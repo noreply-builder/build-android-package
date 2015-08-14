03e8ac47636db41c870033cffb962cd61eb5786d
builder <noreply.watcher@gmail.com>
2015-08-14 05:36:50 -0400

    fix log

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -d "$HOME/android-ndk-r10e" ]; then
+#if [ ! -d "$HOME/android-ndk-r10e" ]; then
     wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
     chmod +x android-ndk-r10e-linux-x86_64.bin
-    ./android-ndk-r10e-linux-x86_64.bin
-fi
+    ./android-ndk-r10e-linux-x86_64.bin >$HOME/ndk.log 2>&1
+#fi
 
