c51a3d9270f9e58a3feda18081c9748957833298
builder <noreply.watcher@gmail.com>
2015-08-14 05:31:23 -0400

    fix 7z

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 if [ ! -d "$HOME/android-ndk-r10e" ]; then
     wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
-    7zr x android-ndk-r10e-linux-x86_64.bin
+    chmod +x android-ndk-r10e-linux-x86_64.bin
+    ./android-ndk-r10e-linux-x86_64.bin
 fi
 
