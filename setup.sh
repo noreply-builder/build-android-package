0508816889fcd24aa369b8155b480ab01596bd8f
builder <noreply.watcher@gmail.com>
2015-08-14 06:12:18 -0400

    fix cache

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+cd $HOME
+
 if [ ! -f "$HOME/android-ndk-r10e-linux-x86_64.bin" ]; then
     wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
     chmod +x android-ndk-r10e-linux-x86_64.bin
 fi
 ./android-ndk-r10e-linux-x86_64.bin >$HOME/ndk.log 2>&1
+cd -
