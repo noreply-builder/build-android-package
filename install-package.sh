7112b971c1351578fa35911daa7c20834ef3d824
builder <noreply.watcher@gmail.com>
2015-08-14 05:13:54 -0400

    add build files

---
+++ install-package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git clone https://github.com/clowwindy/ShadowVPNAndroid.git
+cd ShadowVPNAndroid
+git submodule update --init --recursive
+
+cd -
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -d "$HOME/android-ndk-r10e" ]; then
+    wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
+    7zr x android-ndk-r10e-linux-x86_64.bin
+fi
+
